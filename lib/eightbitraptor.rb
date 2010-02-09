48f7b2b09a97a2ec90bcdaac58e452f65887d295
Matt House <matt@eightbitraptor.com>
2010-02-09 08:47:01 +0000

    STTW approach to posts

--- lib/eightbitraptor.rb
+++ lib/eightbitraptor.rb
@@ -1,9 +1,22 @@
 require 'sinatra'
+require 'redcloth'
+require 'pp'
 
 class EightBitRaptor < Sinatra::Base
   set :views, File.join(File.dirname(__FILE__), *%w[.. views])
   set :public, File.dirname(__FILE__) + '/../public'
+  set :posts_path, File.dirname(__FILE__) + '/../posts'
+  
   enable :static  
+
+  def post(name)
+    path = File.join(options.posts_path, name)
+    config = YAML.load_file("#{path}/meta.yml")
+    
+    {:body  => RedCloth.new(File.read("#{path}/post.markdown")).to_html,
+     :title => config[:title],
+     :date  => config[:date] }
+  end
 
   # 
   # Here lieth controller actions, may they REST in peace
