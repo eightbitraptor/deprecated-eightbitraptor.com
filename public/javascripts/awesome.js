ada949026148ea1c4b8fee2e5bd52faecd14ad24
Matt House <matt@eightbitraptor.com>
2010-06-28 09:05:40 +0100

    Make the carousel pretty.

--- public/javascripts/awesome.js
+++ public/javascripts/awesome.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($){
 
-  var slideWidth = 598;
+  var slideWidth = 600;
 
   $('.slider').prepend('<a id="slider-left" class="slider-nav" href="#"><</a>');
   $('.slider').prepend('<a id="slider-right" class="slider-nav" href="#">></a>');
